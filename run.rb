175f2806d342c66af25a7f37bd0e112fa2b59b55
Harald Sitter <sitter@kde.org>
2017-06-09 15:32:05 +0200

    move serving into contain as we need this on the host not the container

--- run.rb
+++ run.rb
@@ -58,26 +58,6 @@ config = {
 # Neon builders don't do KVM, disable it if the module is not loaded.
 config[:QEMU_NO_KVM] = true unless system('lsmod | grep -q kvm_intel')
 
-require 'webrick'
-s = WEBrick::HTTPServer.new(DocumentRoot: '.', Port: 0)
-Thread.new do
-  shutdown = proc do
-    warn 'shutting down httpserver'
-    s.shutdown
-  end
-  siglist = %w[TERM QUIT]
-  siglist.concat(%w[HUP INT]) if STDIN.tty?
-  siglist &= Signal.list.keys
-  siglist.each do |sig|
-    Signal.trap(sig, shutdown)
-  end
-  warn 'starting server'
-  s.start
-end
-
-warn "Live data @ http://build.neon.kde.org:#{s.config.fetch(:Port)}.\n" \
-  'Disappears on exit.'
-
 File.write('vars.json', JSON.generate(config))
 File.write('live_log', '')
 system({ 'QEMU_AUDIO_DRV' => 'none' },
