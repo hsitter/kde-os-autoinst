52a61108ec634ee36dd159f1752f8a8df3aaa593
Jonathan Riddell <jr@jriddell.org>
2017-10-19 14:19:59 +0100

    keep it loose

--- neon/lib/distribution_neon.pm
+++ neon/lib/distribution_neon.pm
@@ -1,6 +1,5 @@
 package distribution_neon;
 use base 'distribution';
-use strict;
 
 use testapi qw(send_key %cmd assert_screen check_screen check_var get_var match_has_tag set_var type_password type_string wait_idle wait_serial mouse_hide send_key_until_needlematch record_soft_failure wait_still_screen wait_screen_change diag);
 
