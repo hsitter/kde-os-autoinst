e64885cb063b60e9fc2a453d4c051316959e9df3
Harald Sitter <sitter@kde.org>
2017-12-22 11:27:44 +0100

    let gpg key retrieval fail a bunch of times and retry

--- bin/sync.rb
+++ bin/sync.rb
@@ -21,6 +21,24 @@
 
 require 'fileutils'
 
+# NB: stolen from pangea-tooling (same license and copyright)
+# Retry given block.
+# @param tries [Integer] amount of tries
+# @param errors [Array<Object>] errors to rescue
+# @param sleep [Integer, nil] seconds to sleep between tries
+# @param name [String, 'unknown'] name of the action (debug when not silent)
+# @yield yields to block which needs retrying
+def retry_it(times: 1, errors: [StandardError], sleep: nil, silent: false,
+             name: 'unknown')
+  yield
+rescue *errors => e
+  raise e if (times -= 1) <= 0
+  print "Error on retry_it(#{name}) :: #{e}\n" unless silent
+  Kernel.sleep(sleep) if sleep && !@sleep_disabled
+  retry
+end
+
+
 TYPE = ENV.fetch('TYPE')
 ISO_URL = "http://files.kde.org/neon/images/neon-#{TYPE}/current/neon-#{TYPE}-current.iso".freeze
 ZSYNC_URL = "#{ISO_URL}.zsync".freeze
@@ -49,5 +67,6 @@ else # probably not
   system('zsync_curl', '-o', 'neon.iso', ZSYNC_URL) || raise
 end
 system('wget', '-q', '-O', 'neon.iso.sig', SIG_URL) || raise
-system('gpg2', '--recv-key', GPG_KEY) || raise
+# Retry this a bit, gpg servers may not always answer in time.
+retry_it(times: 4, sleep: 1) { system('gpg2', '--recv-key', GPG_KEY) || raise }
 system('gpg2', '--verify', 'neon.iso.sig') || raise
