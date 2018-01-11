768f4ac946a079c87a7687a3f71baf9167a22947
Harald Sitter <sitter@kde.org>
2018-01-11 16:57:59 +0100

    reduce timeout between counter

--- neon/tests/natives/qt_compat.pm
+++ neon/tests/natives/qt_compat.pm
@@ -34,13 +34,13 @@ sub run {
     assert_screen 'folder-desktop', 30;
 
     assert_screen_change { x11_start_program('kdevelop'); };
-    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 30);
+    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 2);
 
     assert_screen_change { x11_start_program('skrooge'); };
-    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 30);
+    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 2);
 
     assert_screen_change { x11_start_program('kontact'); };
-    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 30);
+    send_key_until_needlematch('folder-desktop', 'alt-f4', 20, 2);
 }
 
 1;
