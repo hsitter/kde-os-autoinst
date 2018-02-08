1914b60181ce9b541a50b57c06edda5955c012c8
Harald Sitter <sitter@kde.org>
2018-02-08 12:54:22 +0100

    try more possible locations for the rotten xsession-errors file

--- neon/lib/basetest_neon.pm
+++ neon/lib/basetest_neon.pm
@@ -33,7 +33,9 @@ sub post_fail_hook {
     # The next uploads are largely failok since we want to get as many logs
     # as possible evne if some are missing.
 
+    upload_logs '/home/'.$testapi::username.'/.cache/sddm/xsession-errors', failok => 1;
     upload_logs '/home/'.$testapi::username.'/.cache/xsession-errors', failok => 1;
+    upload_logs '/home/'.$testapi::username.'/.xsession-errors', failok => 1;
 
     script_run 'journalctl --no-pager -b 0 > /tmp/journal.txt';
     upload_logs '/tmp/journal.txt', failok => 1;
