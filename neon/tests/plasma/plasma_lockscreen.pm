08f3d73c653feaf65acc5f763e8ddb444a52718f
Harald Sitter <sitter@kde.org>
2018-03-05 12:58:33 +0100

    use correct base test to get logs archived -.-

--- neon/tests/plasma/plasma_lockscreen.pm
+++ neon/tests/plasma/plasma_lockscreen.pm
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use base "basetest";
+use base "basetest_neon";
 use strict;
 use testapi;
 
