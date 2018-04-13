2cdca8ccfd1999ee51bb1e506bdb920d5cb0b46e
Harald Sitter <sitter@kde.org>
2018-04-13 11:53:57 +0200

    add more debugging to upgrade test
    switching to x11 fails, but according to init_consoles we have the right
    tty set for x11. so, debug everything immediately related to where sddm
    might be to get some context.

--- neon/tests/install/upgrade_bionic.pm
+++ neon/tests/install/upgrade_bionic.pm
@@ -91,6 +91,19 @@ sub run {
     # Make sure to fully reinit the consoles to adopt the VT change.
     $testapi::distri->init_consoles();
     $self->boot_to_dm;
+
+    select_console 'log-console';
+    {
+        script_sudo 'ps aux |grep tty';
+        script_sudo 'ps aux |grep sddm';
+
+        script_sudo "loginctl list-sessions";
+
+        for ($i = 0; $i < 10; $i++) {
+            script_sudo "loginctl show-session $i";
+        }
+    }
+    select_console 'x11';
 }
 
 sub post_fail_hook {
