7439e7475479d4f172bce55ceecf032b7e588718
Harald Sitter <sitter@kde.org>
2018-06-11 13:30:05 +0200

    switch to x11_start_program
    seems our default install doesn't sync up dbus. somehow my system does that
    though. perhaps extra packages *shrug*

--- neon/tests/plasma/plasma_favorite.pm
+++ neon/tests/plasma/plasma_favorite.pm
@@ -26,12 +26,10 @@ sub run {
 
     # Temporary hack to see if disabling compositing is able to make the needles
     # pass. Working theory is that translucency is screwing the matching degree.
-    select_console 'log-console';
-    {
-        assert_script_run 'qdbus org.kde.KWin /Compositor org.kde.kwin.Compositing.suspend';
-    }
-    select_console 'x11';
-
+    x11_start_program 'konsole';
+    assert_screen 'konsole';
+    type_string 'qdbus org.kde.KWin /Compositor org.kde.kwin.Compositing.suspend';
+    send_key 'ret';
 
     # Starts the Application Launcher
     assert_and_click 'kickoff';
