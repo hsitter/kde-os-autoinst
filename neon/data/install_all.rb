c0d5a3ab075c166bbbf2c88d333d7844457d4201
Harald Sitter <sitter@kde.org>
2018-06-20 16:40:12 +0200

    add thrilling new tech which is meant to install all packages in a repo
    this specifically is meant to be a test run before the upgrade test, to
    test an upgrade with all bells and whistles instead of barbones neon

---
+++ neon/data/install_all.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2018 Harald Sitter <sitter@kde.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Install all packages in the neon repo.
+# This is somewhat overlappingly duplicative of pangea-tooling's
+# repo_abstraction, but we don't have access to the tooling inside the VM, and
+# considering the use case its tech is vastly too advanced and hard to use.
+
+begin
+  require 'aptly'
+rescue LoadError
+  raise 'not root' if Process.uid != 0
+  Gem.install('aptly-api')
+  require 'aptly'
+end
+
+Aptly.configure do |config|
+  config.uri = URI::HTTPS.build(host: 'archive-api.neon.kde.org')
+end
+
+sources = File.read('/etc/apt/sources.list.d/neon.list')
+sources = sources.split($/).reject { |x| x.start_with?('#') || x.empty? }
+sources = sources.compact.uniq
+source = sources[0]
+warn "Source: #{source}"
+raise source unless source.start_with?('deb http://archive.neon')
+source_uri = URI.parse(source.split(' ')[1])
+
+pub = Aptly::PublishedRepository.list.find do |x|
+  File.join('/', x.Prefix) == source_uri.path
+end
+packages = []
+pub.Sources.each do |x|
+  packages += x.packages(q: '!Name (~ ".*-[dbg|dbgsym]"), $Architecture (amd64)')
+end
+packages = packages.collect { |x| x.split(' ')[1] }
+# special hack, neon-adwaita in 16.04 is not meant to be installed
+packages = packages.reject { |x| x == 'neon-adwaita' }
+
+ENV['DEBIAN_FRONTEND'] = 'noninteractive'
+system('apt update') || raise
+system('apt', 'install', '-y', *packages) || raise
---
+++ neon/tests/install/install_all_packages.pm
@@ -0,0 +1,48 @@
+# Copyright (C) 2018 Harald Sitter <sitter@kde.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use base "basetest_neon";
+use testapi;
+
+# Doesn't really do much beyond installing all packages. It's meant to be used
+# before more useful tests that would need all packages installed.
+
+sub run {
+    my ($self) = @_;
+    # Do not log in here. basetest can't tell if a boot has happened, so it
+    # must finde the DM to know what it needs to do when a subsequent test calls
+    # `boot` on it.
+    $self->boot_to_dm;
+
+    select_console 'log-console';
+    {
+        assert_script_run 'wget ' . data_url('install_all.rb'),  16;
+        assert_script_sudo 'ruby insall_all.rb', 60 * 45;
+    }
+    select_console 'x11';
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { important => 1, fatal => 1 };
+}
+
+1;
