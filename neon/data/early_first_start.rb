851e5791620d62493e71ed3d1e43abf54f454faf
Harald Sitter <sitter@kde.org>
2018-06-20 16:28:50 +0200

    point to more in depth information

--- neon/data/early_first_start.rb
+++ neon/data/early_first_start.rb
@@ -36,6 +36,7 @@ system 'apt update' || raise
 system 'apt install -y systemd-coredump' || raise
 
 # ttyS1 is set up by our kvm wrapper, it ordinarily isn't available
+# Also see bin/kvm_arg_injector for additional information.
 puts "#{$0} Letting systemd-journald log to ttyS1."
 system 'sed -i "s%.*ForwardToConsole=.*%ForwardToConsole=yes%g" /etc/systemd/journald.conf' || raise
 system 'sed -i "s%.*TTYPath=.*%TTYPath=/dev/ttyS1%g" /etc/systemd/journald.conf' || raise
