6b05a1393689587e78cb2950d191de592bae4b13
Harald Sitter <sitter@kde.org>
2018-08-28 13:57:06 +0200

    enable logging for the upgrade notifier
    otherwise it's hard to figure out what went wrong if the notification
    doesn't come up

--- neon/tests/install/upgrade_bionic.pm
+++ neon/tests/install/upgrade_bionic.pm
@@ -93,6 +93,15 @@ sub run {
     }
     select_console 'x11';
 
+    select_console 'log-console';
+    {
+        # Install a suitable logging file so we can easily debug things in case
+        # of failure.
+        assert_script_run 'mkdir -pfv .config/QtProject/'
+        assert_script_run 'wget -O .config/QtProject/qtlogging.ini ' . data_url('upgrade_bionic_qtlogging.ini');
+    }
+    select_console 'x11';
+
     $self->login;
     assert_screen 'folder-desktop', 30;
 
