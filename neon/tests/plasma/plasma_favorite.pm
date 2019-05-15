44010b0c5efc7f6a7fe771e31c4fa98547c4986d
Harald Sitter <sitter@kde.org>
2019-05-15 15:01:21 +0200

    Revert "reduce sleep overhead"
    This reverts commit 87cead6dc957c5968253f032a9be3e79c578f304.

--- neon/tests/plasma/plasma_favorite.pm
+++ neon/tests/plasma/plasma_favorite.pm
@@ -64,7 +64,7 @@ sub assert_shimmy_and_click {
     bmwqemu::diag("clicking at $x/$y");
     mouse_set($x, $y);
     mouse_set($x-1, $y);
-    sleep 1; # Give a chance ot receive the movement events
+    sleep 16; # Give a chance ot receive the movement events
     mouse_set($x+1, $y);
 
     if ($args{dclick}) {
