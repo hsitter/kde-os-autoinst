f2e34d4b19bbcc6bb0f0b92db3d6d45f5cea81a1
Harald Sitter <sitter@kde.org>
2020-05-04 12:21:37 +0200

    rename upgrade_bionic to focal as initial upgrade test for focal
    hasn't run yet, so no clue if it works

--- neon/tests/install/upgrade_focal.pm
+++ neon/tests/install/upgrade_focal.pm
@@ -1,20 +1,5 @@
-# Copyright (C) 2018 Harald Sitter <sitter@kde.org>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License or (at your option) version 3 or any later version
-# accepted by the membership of KDE e.V. (or its successor approved
-# by the membership of KDE e.V.), which shall act as a proxy
-# defined in Section 14 of version 3 of the license.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-FileCopyrightText: 2018-2020 Harald Sitter <sitter@kde.org>
+# SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 use base "basetest_neon";
 use testapi;
@@ -180,7 +165,7 @@ sub run {
     # Switch to bionic mode now.
     # This among other things makes sure the right virtual terminals will be
     # used for x11 etc.
-    set_var 'OPENQA_SERIES', 'bionic', reload_needles => 1;
+    set_var 'OPENQA_SERIES', 'focal', reload_needles => 1;
     console('x11')->set_tty(1);
     reset_consoles;
 
